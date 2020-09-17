177947b495a8812d2db540a066f3e2f0a1b53fc2
Krzysiek <chrisphilipniznik@gmail.com>
2020-09-17 15:35:48 +0200

    Replace server type of reading txt file to upload on client only side

---
+++ dist/hashers/HashMap.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=HashMap.js.map
---
+++ dist/utils/Input parser.js
@@ -0,0 +1,6 @@
+var InputParser;
+(function (InputParser) {
+    inputWord: HTMLElement = document.getElementsByClassName('form-control');
+    buttonPrint: HTMLElement = document.getElementsByClassName('btn btn-primary');
+})(InputParser || (InputParser = {}));
+//# sourceMappingURL=Input parser.js.map
