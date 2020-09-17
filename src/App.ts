05fac91e01dd8874a6490674bc329173e5b8db08
Ozukyo <ozukyo@gmail.com>
2020-09-17 22:04:49 +0200

    Add appearing word suggesitons on page

--- src/App.ts
+++ src/App.ts
@@ -1,70 +1,27 @@
 import {CsvParser} from "./utils/CsvParser.js";
 import {WordChecker} from "./utils/WordChecker.js";
-import {BetterStringHasher} from "./hashers/BetterStringHasher";
-import {LousyStringHasher} from "./hashers/LousyStringHasher";
-import {DegenerateStringHasher} from "./hashers/DegenerateStringHasher";
-// import {HashTable} from "./utils/HashTable";
-// import {UserInputReader} from "./utils/UserInputReader.js";
 
-// const betterStringHasher = new BetterStringHasher().hash('dupa');
-// const lousyStringHasher = new LousyStringHasher().hash('dupa');
-// const degenerateStringHasher = new DegenerateStringHasher().hash('dupa');
-//
-// console.log(betterStringHasher);
-// console.log(lousyStringHasher);
-// console.log(degenerateStringHasher);
-// function main(): void {
-//     let userInput: UserInputReader = new UserInputReader()
-//     userInput.getInput();
-//     console.log('test');
-//     let data = new HashTable();
-//     data.convertToHashTable();
-// // userInput.getInput();
-//     console.log(data.getItem("a"));
-//     console.log(data.getItem("accept"));
-//     console.log(data.getItem("venom"));
-//     console.log(data.lookup("venom"));
-//     const dupa = new UserInputReader();
-//     dupa.getInput();
-// const tablica:string[] = [];
-//     const input: HTMLInputElement = document.querySelector('input[type="file"]')!
-//     input.addEventListener('change',function(e){
-//             console.log(input.files)
-//             const reader = new FileReader()
-//             reader.onload = function(){
-//                 if (typeof reader.result === "string") {
-//                     tablica.push(reader.result);
-//                 }
-//             }
-//             // @ts-ignore
-//         reader.readAsText(input.files[0])
-//         }, false
-//     )
+const init = () => {
+    const csvParser = new CsvParser();
+    csvParser.readFile();
+    const button: HTMLElement = document.getElementById('btn-summary')!;
 
-// const fs = require("fs");
-//
-// function parseDataToArray()
-// {
-//     return fs.readFileSync('../wordlist.txt')
-//         .toString() // convert Buffer to string
-//         .split('\n') // split string to lines
-//         .map((e: string) => e.trim());
-// }
-//     // const csvParser = CsvParser.parseDataToArray();
-//     console.log(parseDataToArray());
+    button.addEventListener('click', function () {
+        const wordChecker = new WordChecker(csvParser.lines);
+        const inputValue: string = (<HTMLInputElement>document.getElementById('ex1')).value;
+        let p = document.querySelector("p");
+        p.innerText = "DID YOU PERHAPS MEAN:"
+        addSuggestions(wordChecker.checkInsertingLetterInEachPositionOfTheString(inputValue));
+    })
 
-// const userInputReader = new UserInputReader();
-// const dupa = userInputReader.getInput();
+    function addSuggestions(listOfWords: string[]) {
+        let ulElement = document.querySelector("ul");
+        listOfWords.forEach(word => {
+            let liElement = document.createElement("li");
+            liElement.innerText = word;
+            ulElement.appendChild(liElement);
+        });
+    }
+}
 
-const csvParser = new CsvParser();
-csvParser.readFile();
-// const wordChecker = new WordChecker(csvParser.lines);
-const button: HTMLElement = document.getElementById('btn-summary')!;
-
-
-button.addEventListener('click', function () {
-    const wordChecker = new WordChecker(csvParser.lines);
-    const inputValue: string = (<HTMLInputElement>document.getElementById('ex1')).value;
-    console.log(wordChecker.checkInsertingLetterInEachPositionOfTheString(inputValue))
-
-})
+init();
